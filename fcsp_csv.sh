14d26ec1ef453a84dec0079e3e72aa87213acbae
Dmitry Olshansky <dmitry.olsh@gmail.com>
2014-11-11 03:01:28 +0300

    New decriptor, cleanup and better CSV wrapper

--- fcsp_csv.sh
+++ fcsp_csv.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 NAME=/tmp/fcsp.codes
-./fcsp -d . $1 > $NAME 2>/tmp/fcsp.log
-FILE=`echo $1 | sed -r 's|.*/(.*)|\1|'`
-(echo $FILE; wc -l $NAME | sed -r 's/([0-9]+)\s*.*/\1/'; (cat $NAME | tr '\n' ' ')) | tr '\n' ';'
+DESC_DIR="." # load descriptors from this directory
+if [ $# -lt  1 ] ; then
+	exit 1
+fi
+for f in $@ ; do
+	MOL=`echo $f | sed -r 's|.*/(.*)|\1|'`
+	./fcsp -d $DESC_DIR $f > $NAME 2>/tmp/fcsp.log
+	FILE=`echo $f | sed -r 's|.*/(.*)|\1|'`
+	(echo $MOL; wc -l $NAME | sed -r 's/([0-9]+)\s*.*/\1/'; (cat $NAME | tr '\n' ' ')) | tr '\n' ';'
+	echo
+done
