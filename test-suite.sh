900ab7a217289860a8fa879d8492e878953d3cd1
Dmitry Olshansky <dmitry.olsh@gmail.com>
2015-10-09 16:25:15 +0300

    Tweak test cases

--- test-suite.sh
+++ test-suite.sh
@@ -2,8 +2,9 @@
 LOG_LEVEL=3 # warn-s and worse
 for t in tests/* ; do
 	echo "Encoding" `echo -n $t | sed -r 's|.*/(.*)|\1|'`
-	FILES=`echo $t/MOL/*.{mol,MOL} | sort`
-	./fcss-2a -v ${LOG_LEVEL} --format=csv -d . $FILES > $t/fcss-2a-dev.csv
+	FILES=`echo $t/MOL/*.MOL | sort`
+#	echo $FILES
+	./fcss-2a -v ${LOG_LEVEL} --format=csv $FILES > $t/fcss-2a-dev.csv
 done 2>test-suite.log
 
 for t in tests/* ; do
