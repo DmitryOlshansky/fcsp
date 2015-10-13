98bad5b4eae3d297ef0598a3f36dde81fce9809a
Dmitry Olshansky <olshansky@knackit.com>
2015-10-13 10:44:43 +0300

    Add "common" summary line to fcss-comp; investigate bug in processing many mols at a time

--- test-suite.sh
+++ test-suite.sh
@@ -4,10 +4,10 @@ for t in tests/* ; do
 	echo "Encoding" `echo -n $t | sed -r 's|.*/(.*)|\1|'`
 	FILES=`echo $t/MOL/*.MOL | sort`
 #	echo $FILES
-	./fcss-2a -v ${LOG_LEVEL} --format=csv $FILES > $t/fcss-2a-dev.csv
+	echo "$FILES" | xargs -n 2 ./fcss-2a -v ${LOG_LEVEL} --format=csv  > $t/fcss-2a-dev.csv
 done 2>test-suite.log
 
 for t in tests/* ; do
 	echo "Comparing " `echo -n $t | sed -r 's|.*/(.*)|\1|'`
-	./fcss-comp -i $t/fcss-2.csv $t/fcss-2a-dev.csv | tee $t/diff.cmp | grep -A 5 "SUMMARY" | tail -2
+	./fcss-comp -i $t/fcss-2.csv $t/fcss-2a-dev.csv | tee $t/diff.cmp | grep -A 5 "SUMMARY" | tail -3
 done
