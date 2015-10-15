362aa8a079b987d7c143048e567e1ba895c1b8d7
Dmitry Olshansky <dmitry.olsh@gmail.com>
2015-10-15 14:15:39 +0300

    Update report.sh, our CSV generator

--- report.sh
+++ report.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-echo "Выборка,Только в ФКСП-2,Только в ФКСП-2А,Общие для ФКСП-2 и ФКСП-2А"
+echo "Выборка,Соединений,Только в ФКСП-2,Только в ФКСП-2А,Общие Дескрипторы"
 for t in tests/* ; do
+	MOLS=`find $t/MOL -name '*.MOL' | wc -l`
 	DATA=`fcss-comp -i $t/fcss-2.csv $t/fcss-2a.csv |  grep -A 5 "SUMMARY" | tail -3`
 	COUNTS=`echo "$DATA" | cut -f2 -d ':'`
-	echo -n $t
+	echo -n $t,$MOLS
 	for f in $COUNTS ; do
 		echo -n ",$f"
 	done
